2ebd1610c5a58a430f2877b7cfc4015d3e744486
Valdielson Siqueira <valdielson.silva@gmail.com>
2022-12-15 22:49:56 -0300

    feat(product): adicionando components

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,33 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme,
+  config,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fonts,
+    fontSizes,
+    lineHeights,
+    radii,
+    space,
+  },
+})
